d85c15b0a3da3e2e09d062c28bce0a5a46ce279a
MarcosRBasso <39345265+MarcosRBasso@users.noreply.github.com>
2022-05-30 09:45:48 -0300

    Add files via upload

--- menu_functions.py
+++ menu_functions.py
@@ -87,6 +87,16 @@ def enviar_dados_logradourosBairros():
         send_log_info("Não foram encontrados registros na tabela logradourosBairros para enviar.")
     send_log_info("Processamento dos dados da tabela logradourosBairros finalizado.")
 
+#def enviar_dados_Bairros():
+ #   send_log_info("Iniciando o processamento dos dados da tabela Bairros.")
+  #  listaBairros = Bairros.db_list()
+   # if listaBairros:
+    #    for res in listaBairros:
+     #       Bairros.send_post(res[0], res[1], res[2], res[3])
+    #else:
+    #    send_log_info("Não foram encontrados registros na tabela Bairros para enviar.")
+    #send_log_info("Processamento dos dados da tabela Bairros finalizado.")    
+
 def enviar_dados_advogados():
     send_log_info("Iniciando o processamento dos dados da tabela advogados.")
     listaadvogados = advogados.db_list()
@@ -146,7 +156,7 @@ def enviar_dados_agrupamentos():
     listaagrupamentos = agrupamentos.db_list()
     if listaagrupamentos:
         for res in listaagrupamentos:
-            agrupamentos.send_post(res[0], res[1], res[2])
+            agrupamentos.send_post(res[0], res[1], res[2], res[3])
     else:
         send_log_info("Não foram encontrados registros na tabela agrupamentos para enviar.")
     send_log_info("Processamento dos dados da tabela agrupamentos finalizado.")
